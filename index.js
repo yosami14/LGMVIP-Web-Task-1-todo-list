61e0133095992bce0c563f0ee188785525280c5c
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2023-06-20 11:36:40 +0300

    modified the logic

--- index.js
+++ index.js
@@ -4,12 +4,42 @@ btnListener.addEventListener('click', addListFunction);
 function addListFunction() {
   const inputValue = document.querySelector('#listInput').value;
   const addList = document.querySelector('.addedList');
-  //CREATE LIST
+  
+  // CREATE LIST DIV
   const listDiv = document.createElement('div');
-  listDiv.textContent = inputValue;
-  listDiv.classList.add('lists')
+  listDiv.classList.add('lists');
 
-  //CREATE DELETE BTN AND CHECK BTN
-  const deleteBtn = '<i class="fa-solid fa-xmark"></i>' ;
+  // CREATE CHECKBOX
+  const checkBtn = document.createElement('input');
+  checkBtn.type = 'checkbox';
+  checkBtn.classList.add('checkList');
+  listDiv.appendChild(checkBtn);
+
+  // Adding Input Value
+  const toDotext = document.createElement('span');
+  toDotext.classList.add('toDoText');
+  toDotext.textContent = inputValue;
+  listDiv.appendChild(toDotext);
+
+  // CREATE DELETE BUTTON
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  deleteBtn.classList.add('deleteList');
+  listDiv.appendChild(deleteBtn);
+
+  // Appending Div
   addList.appendChild(listDiv);
+
+  // Add event listeners to delete button and checkbox
+  deleteBtn.addEventListener('click', function() {
+    listDiv.remove(); // Remove the corresponding listDiv when delete button is clicked
+  });
+
+  checkBtn.addEventListener('change', function() {
+    if (checkBtn.checked) {
+      toDotext.style.textDecoration = 'line-through'; // Apply strikethrough when checkbox is checked
+    } else {
+      toDotext.style.textDecoration = 'none'; // Remove strikethrough when checkbox is unchecked
+    }
+  });
 }
